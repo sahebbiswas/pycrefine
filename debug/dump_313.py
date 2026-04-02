80f3ce478741d1cbe5c56f653b547ceb80dd07cf
PteroDactyl <sahebbiswas@gmail.com>
2026-04-01 20:32:05 -0600

    refactor: remove unused and deprecated utility files

--- debug/dump_313.py
+++
@@ -1,5 +0,0 @@
-import dis
-import simple
-
-print("Python version:", __import__('sys').version)
-dis.dis(simple)
--- debug/dump_std.py
+++
@@ -1,8 +0,0 @@
-import dis
-
-def show_info(x: int) -> int:
-    for i in range(x):
-        print(2*i)
-    return 2%x == 0
-
-dis.dis(show_info)
